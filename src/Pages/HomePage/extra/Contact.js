9d73d9e69a3159e9e65da4bbca9fbe99d4568dee
mintusarker <sarkermintu707@gmail.com>
2023-08-14 17:20:08 +0600

    modified

--- src/Pages/HomePage/extra/Contact.js
+++ src/Pages/HomePage/extra/Contact.js
@@ -15,9 +15,9 @@ const Contact = () => {
                 <p className='pb-1 text-3xl font-semibold font-serif inline border-b-4 border-gray-500'>Contact Us</p>
                 <p className='pt-6'>Submit the form to get in touch with us</p>
             </div>
-            <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 px-16 lg:px-40 py-10 gap-9 items-baseline'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 px-16 lg:px-40 py-10 gap-9 items-baseline'>
 
-                <div className='pl-32'>
+                <div className='lg:ml-16'>
                     <div className='py-8'>
                         <p className='text-3xl mb-6'>Have a question?</p>
                         <span className='text-amber-700 text-lg'>Email me at mintosarker707@gmail.com</span>
@@ -31,7 +31,7 @@ const Contact = () => {
                 </div>
 
                 <div>
-                    <form action="" onSubmit={handleSubmit} className='flex flex-col  w-full'>
+                    <form action="" onSubmit={handleSubmit} className='flex flex-col w-auto'>
                         <input type="text" name='name' placeholder='Enter your name' className='p-2 border-2 border-black rounded-md focus:outline-none' required />
                         <input type="email" name='name' placeholder='Enter your email' className='p-2 my-3  border-2 border-black  rounded-md focus:outline-none' required />
 
